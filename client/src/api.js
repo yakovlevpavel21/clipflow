45fd5ed456d2480cfef50a2f7c5d79fcbb89cfe0
yakovlevpavel21 <pashok20032012@mail.ru>
2026-03-28 17:02:46 +0300

    и снова

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL || '';
 
 // Базовая настройка axios
-axios.defaults.baseURL = API_URL;
+axios.defaults.baseURL = '';
 
 // Автоматическая подстановка токена
 axios.interceptors.request.use(config => {
